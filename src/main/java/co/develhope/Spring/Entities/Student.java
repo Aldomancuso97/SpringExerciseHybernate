42cbeccf352e9cfa872e262ff3a5f98fa27c2021
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-03-18 16:37:03 +0100

    esercizio mock db

--- src/main/java/co/develhope/Spring/Entities/Student.java
+++ src/main/java/co/develhope/Spring/Entities/Student.java
@@ -1,7 +1,6 @@
 package co.develhope.Spring.Entities;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table
@@ -9,56 +8,17 @@ public class Student {
 
     @Id
     @GeneratedValue
-    private int ID;
-    @Column(name = "students_name", nullable = false)
+    private long Id;
+
+    @Column(name = "Student_name", nullable = false)
     private String firstName;
-    @Column(name = "students_lastName", nullable = false)
+
+    @Column(name = "Student_lastName", nullable = false)
     private String lastName;
-    @Column(name = "students_email",nullable = false, unique = true)
+
+    @Column(name = "Student_email", unique = true)
     private String email;
 
-    @OneToMany(mappedBy = "student")
-    private List<Enrollment> enrollmentList;
 
-    public Student() {
-    }
 
-    public Student(int ID, String firstName, String lastName, String email) {
-        this.ID = ID;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
